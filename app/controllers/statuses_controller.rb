39433e573e676ef189ae85ff0b50856f64dc17d3
Andrew Buckingham <abuckingham99@gmail.com>
2015-05-12 23:13:21 -0500

    Add ability to post new status messages

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -4,6 +4,22 @@ class StatusesController < ApplicationController
   end
 
   def create
-    
+   @status = Status.new(status_params) 
+   user = User.find(1)
+   @status.creator = user
+
+   if @status.save
+     flash[:notice] = "Status created!"
+     redirect_to user_path(user.username)
+   else
+     render :new
+   end
+  end
+
+  
+  private
+
+  def status_params
+    params.require(:status).permit(:body)
   end
 end
