32d1e54cd2476a5530481eaf011a8d0baefe325d
Andrew Buckingham <abuckingham99@gmail.com>
2015-05-17 00:23:03 -0500

    Add session#create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    params[:username]
-    params[:password]
+    user = User.find_by username: params[:username]
+    if user && user.authenticate(params[:password])
+
+      flash[:notice] = "Welcome, you're logged in!"
+      redirect_to user_path(user)
+    else
+      flash.now[:error] = "There is something wrong with your username or password."
+      render :new
+    end
   end
 end
