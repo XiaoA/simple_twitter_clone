03e881840860f93e6e6bd80da6eda8dbc3b83e46
Andrew Buckingham <abuckingham99@gmail.com>
2015-05-16 23:49:11 -0500

    Add sessions#new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
 #  root to: 'users#index'
 
   resources :users, only: [:new, :create]
