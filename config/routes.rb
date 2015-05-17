da2dc65be77cc7c94d3398c49941c396c938299b
Andrew Buckingham <abuckingham99@gmail.com>
2015-05-17 12:21:04 -0500

    Add logged_in? and current_user helper methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-
+  get '/logout', to: 'sessions#destroy'
+  
 #  root to: 'users#index'
 
   resources :users, only: [:new, :create]
